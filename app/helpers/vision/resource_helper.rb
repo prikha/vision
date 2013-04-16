671ba6978ca7e77c66fa3b1b235ee621192ecff3
justvitalius <justvitalius@gmail.com>
2013-04-16 21:20:38 +0400

    + добавлены кнопки сортировки к таблицам

--- app/helpers/vision/resource_helper.rb
+++ app/helpers/vision/resource_helper.rb
@@ -1,9 +1,12 @@
 module Vision::ResourceHelper
   def sortable(column, title = nil)
     title ||= resource_class.human_attribute_name(attr)
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    css_class = column == sort_column ? "current #{sort_direction} link-no-decoration" : 'link-no-decoration'
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    link_to title, params.merge(:sort => column, :direction => direction, :page=>nil), {:class => css_class}
+    direction_class = column == sort_column && sort_direction == "asc" ? "icon-sort-up" : "icon-sort-down"
+    link_to params.merge(:sort => column, :direction => direction, :page=>nil), {:class => css_class} do
+      content_tag(:span, title) + content_tag(:i, '', class: direction_class)
+    end
   end
 
 
